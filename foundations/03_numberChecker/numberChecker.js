c8ff18f189bd664164ec13aa49f774d717804197
diitech <123456789+odinusers.noreply.github.com>
2026-03-26 02:19:37 +0100

    Complete the exercise from 1 to 3 and run the npm test on each exercises.

--- foundations/03_numberChecker/numberChecker.js
+++ foundations/03_numberChecker/numberChecker.js
@@ -1,10 +1,18 @@
 function numberChecker(number) {
-  if (number === 6) {
+  if (number === 1000) {
     return true;
-  } else {
+  } else if (number === 10) {
+    return true;
+  } else if (number === 9){
+    return false;
+  } else if (number === 6) {
     return false;
   }
 }
+numberChecker(1000);
+numberChecker(10);
+numberChecker(10);
+numberChecker(7);
 
 // Do not edit below this line
 module.exports = numberChecker;
