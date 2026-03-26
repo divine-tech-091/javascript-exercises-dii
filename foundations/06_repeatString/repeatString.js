75f49239ce692026d8f7654b8923c414b8333c20
diitech <123456789+odinusers.noreply.github.com>
2026-03-26 17:05:42 +0100

    Complete the exercise 06 and run npm test on each statement of the code.

--- foundations/06_repeatString/repeatString.js
+++ foundations/06_repeatString/repeatString.js
@@ -1,6 +1,22 @@
-const repeatString = function() {
+const repeatString = function(string, num) {
+  
+  let strings = '';
+  for (let index = 0; index < num; index++) {
+     strings += string;
+  }
+   
+   if(num === -1) {
+    return 'ERROR'; 
+  }
+  return strings;
 
 };
+// const number = Math.floor(Math.random() * 1000);
 
+repeatString(' ', 10);
+
+// for (let index = 0; index < number; index++) {
+//  repeatString();
+// }
 // Do not edit below this line
 module.exports = repeatString;
