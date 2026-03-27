a933272722e7c036879f2a24d792f3c92aa158f6
diitech <123456789+odinusers.noreply.github.com>
2026-03-27 22:34:39 +0100

    Complete the 08 exercise and run the npm test on each statement.

--- foundations/08_removeFromArray/removeFromArray.js
+++ foundations/08_removeFromArray/removeFromArray.js
@@ -1,5 +1,14 @@
-const removeFromArray = function() {
-};
+const removeFromArray = function(Arr, ...Arg) {
 
+ const removeItem = new Set(Arg);
+ 
+ let ArrRemove = Arr.filter((item) => { 
+  return !removeItem.has(item) 
+ });
+  
+ return ArrRemove;
+};
+const initial = [1, 2, 3];
+removeFromArray(initial,  "1", 3);
 // Do not edit below this line
 module.exports = removeFromArray;
