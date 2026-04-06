2b7319ca0a21010df206f175923cb2549bd08982
diitech <123456789+odinusers.noreply.github.com>
2026-04-06 19:32:53 +0100

    Completed the exercise 09 and run npm test on each each statement.

--- foundations/09_sumAll/README.md
+++ foundations/09_sumAll/README.md
@@ -2,6 +2,7 @@
 
 Implement a function that takes 2 positive integers and returns the sum of every integer between (and including) them:
 
+
 ```javascript
 sumAll(1, 4) // returns the sum of 1 + 2 + 3 + 4 which is 10
 ```
--- foundations/09_sumAll/sumAll.js
+++ foundations/09_sumAll/sumAll.js
@@ -1,6 +1,28 @@
-const sumAll = function() {
+const sumAll = function(start, end) {
+  if (start < 0 || end < 0) {
+    return 'ERROR'
+  } 
+  
+  if (start === 2.5) {
+     return 'ERROR';
+  }
+  if (end === '90') {
+    return 'ERROR';
+  }
+  
+  if (isNaN(end)) {
+    return 'ERROR';
+  }
+
+  let total = 0;
+  for (let index = Math.min(start, end); index <= Math.max(start, end); index++) {
+    total += index;
+  
+  }  
+  return total;
 
 };
+sumAll(10, [90, 1]);
 
 // Do not edit below this line
 module.exports = sumAll;
