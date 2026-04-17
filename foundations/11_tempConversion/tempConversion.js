3a9872a836fccf9ed9c72e377b73980cbafff9b7
diitech <123456789+odinusers.noreply.github.com>
2026-04-17 06:48:44 +0100

    Completed the exercise 11 and run the npm test On each statement.

--- foundations/11_tempConversion/tempConversion.js
+++ foundations/11_tempConversion/tempConversion.js
@@ -1,11 +1,16 @@
-const convertToCelsius = function() {
+const convertToCelsius = function (fahrenheit) {
+	return Math.round((5 / 9) * (fahrenheit - 32) * 10) / 10;
 };
+convertToCelsius();
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function (celsius) {
+	let convert = (celsius * (9 / 5) + 32).toFixed(1);
+	let isConvert = +convert;
+	return isConvert;
 };
-
+convertToFahrenheit();
 // Do not edit below this line
 module.exports = {
-  convertToCelsius,
-  convertToFahrenheit
+	convertToCelsius,
+	convertToFahrenheit,
 };
