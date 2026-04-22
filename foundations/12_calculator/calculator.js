9f72f1e20ee3f800f6e92748b2dff63a335989b9
diitech <123456789+odinusers.noreply.github.com>
2026-04-22 08:58:19 +0100

    Completed the exercise 12 and run the npm test On each statement.

--- foundations/12_calculator/calculator.js
+++ foundations/12_calculator/calculator.js
@@ -1,33 +1,53 @@
-const add = function() {
-	
+const add = function (num, para) {
+	let isAdd = num + para;
+	return isAdd;
+};
+add();
+
+const subtract = function (num, val) {
+	let isSub = num - val;
+	return isSub;
+};
+subtract();
+
+const sum = function (arr) {
+	// let arr = [7];
+	let sumArr = arr.reduce((acc, val) => {
+		return acc + val;
+	}, 0);
+	return sumArr;
+};
+sum([]);
+
+const multiply = function (elements) {
+	let num = elements.reduce((acc, num) => {
+		return acc * num;
+	});
+	return num;
+};
+multiply([2, 4]);
+
+const power = function (val, num) {
+	let exponent = Math.pow(val, num);
+	return exponent;
+};
+power(4, 3);
+
+const factorial = function (val) {
+	if (val === 0) {
+		return 1;
+	}
+	return val * factorial(val - 1);
 };
 
-const subtract = function() {
-	
-};
-
-const sum = function() {
-	
-};
-
-const multiply = function() {
-
-};
-
-const power = function() {
-	
-};
-
-const factorial = function() {
-	
-};
+factorial(0);
 
 // Do not edit below this line
 module.exports = {
-  add,
-  subtract,
-  sum,
-  multiply,
-  power,
-  factorial
+	add,
+	subtract,
+	sum,
+	multiply,
+	power,
+	factorial,
 };
