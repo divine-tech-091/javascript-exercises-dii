2936540c3ac9cfea32dd9f0eae73685809f4b7eb
diitech <123456789+odinusers.noreply.github.com>
2026-04-26 18:49:24 +0100

    Completed the exercise 13 and run the npm test on each statement.

--- foundations/13_palindromes/palindromes.js
+++ foundations/13_palindromes/palindromes.js
@@ -1,6 +1,12 @@
-const palindromes = function () {
+const palindromes = function (isString) {
+	let ConvertStr = isString.toLowerCase().replace(/[^a-z0-9]/g, '');
 
+	let splitStr = ConvertStr.split('').reverse().join('');
+
+	let crossCheck = ConvertStr === splitStr;
+	return crossCheck;
 };
+palindromes('');
 
 // Do not edit below this line
 module.exports = palindromes;
