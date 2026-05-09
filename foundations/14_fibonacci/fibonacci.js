3923d06aa132d07f9cb9515b0e5064ade4552610
diitech <123456789+odinusers.noreply.github.com>
2026-05-09 10:08:10 +0100

    Completed the exercise 14_fibonacci and run the npm test on each statement.

--- foundations/14_fibonacci/fibonacci.js
+++ foundations/14_fibonacci/fibonacci.js
@@ -1,6 +1,24 @@
-const fibonacci = function() {
+const fibonacci = function (value) {
+	// check the argument is not a number
+	// make sure i worked with number throughout
+	// the function.
+	if (!isNaN(value)) {
+		value = Number(value);
+	}
+	if (value < 0) {
+		return 'OOPS';
+	}
 
+	if (value === 0) {
+		return 0;
+	}
+	if (value <= 2) {
+		return 1;
+	}
+	// recursive function for returning large numbers
+	return fibonacci(value - 1) + fibonacci(value - 2);
 };
+fibonacci('5');
 
 // Do not edit below this line
 module.exports = fibonacci;
