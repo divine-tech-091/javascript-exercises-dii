0a695b32d4ee97711e734424408a7a64d675755f
diitech <123456789+odinusers.noreply.github.com>
2026-05-09 20:57:54 +0100

    Completed the exercise 15_getTheTitle and run the npm test

--- foundations/15_getTheTitles/getTheTitles.js
+++ foundations/15_getTheTitles/getTheTitles.js
@@ -1,6 +1,23 @@
-const getTheTitles = function() {
-
+const getTheTitles = function () {
+	const books = [
+		{
+			title: 'Book',
+			author: 'Name',
+		},
+		{
+			title: 'Book2',
+			author: 'Name2',
+		},
+	];
+	let arr = [];
+	const isBook = books.filter((element) => {
+		if (element !== element.title) {
+			arr.push(element.title);
+		}
+	});
+	return arr;
 };
+getTheTitles();
 
 // Do not edit below this line
 module.exports = getTheTitles;
