806871bc3d012e7dc6212fd0392020dd0f4bc94d
diitech <123456789+odinusers.noreply.github.com>
2026-05-13 21:48:50 +0100

    Completed the exercise 16_findTheOldest, the last exercise on the JavaScript-exercises and run the npm test.

--- foundations/16_findTheOldest/findTheOldest.js
+++ foundations/16_findTheOldest/findTheOldest.js
@@ -1,6 +1,32 @@
-const findTheOldest = function() {
+const findTheOldest = function (people) {
+	let currentYear = new Date().getFullYear();
 
+	let getAge = people.reduce((oldest, current) => {
+		let oldAge = (oldest.yearOfDeath || currentYear) - oldest.yearOfBirth;
+		let currentAge = (current.yearOfDeath || currentYear) - current.yearOfBirth;
+
+		return oldAge < currentAge ? current : oldest;
+	});
+	return getAge;
 };
+const people = [
+	{
+		name: 'Carly',
+		yearOfBirth: 1942,
+		yearOfDeath: 1970,
+	},
+	{
+		name: 'Ray',
+		yearOfBirth: 1962,
+		yearOfDeath: 2011,
+	},
+	{
+		name: 'Jane',
+		yearOfBirth: 1912,
+		yearOfDeath: 1941,
+	},
+];
+findTheOldest(people);
 
 // Do not edit below this line
 module.exports = findTheOldest;
