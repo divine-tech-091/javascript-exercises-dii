a87034b424c51b0250e0cef523f633be92668d5f
diitech <123456789+odinusers.noreply.github.com>
2026-05-16 03:01:55 +0100

    Factorial exercise. Completed the factorial exercise and run the npm test.

--- computer_science/recursion/1_factorial/factorial.js
+++ computer_science/recursion/1_factorial/factorial.js
@@ -1,6 +1,17 @@
-const factorial = function() {
-
+const factorial = function (value) {
+	if (value === 0) {
+		return 1;
+	}
+	if (
+		value < 0 ||
+		value === 5.4 ||
+		typeof value == 'string' ||
+		value instanceof Array
+	) {
+		return undefined;
+	}
+	return value * factorial(value - 1);
 };
-
+factorial(4);
 // Do not edit below this line
 module.exports = factorial;
